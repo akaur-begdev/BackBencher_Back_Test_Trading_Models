7a2b94cf21b9923add4414e0cc2d6a9ecb56238f
akaur-begdev <akaur@qurioskill.ca>
2025-09-13 11:02:54 -0400

    Added Backtesting Strategy

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import yfinance as yf
+import pandas
 
-
+print("-----")
 ticker = 'AAPL'
 data = yf.download(ticker, start='2020-01-01', end='2025-01-01')
 print(data.info())
 print(data.head())
 
 print("-----")
-print(type(data))
+type_of_data = type(data)
+print(f"The data type is {type_of_data}.")
+
+print("-----")
+closing_data = data[["Close"]]
+print(closing_data)
+
+print("-----")
+data["MarketRet"] = data["Close"].pct_change()
+print(data)
+
+print("-----")
+data["Action"] = data["MarketRet"]>0
+print(data)
+
+print("-----")
+data["Action"] = data["Action"].astype(int)
+print(data)
+
+print("-----")
+data["StratRet"] = data["MarketRet"] * data["Action"]
+print(data)
+
+print("-----")
+data['MarketEquity'] = (1 + data['MarketRet']).cumprod()
+data['StratEquity']  = (1 + data['StratRet']).cumprod()
+print(data)
+
+print("-----")
+
 
 
